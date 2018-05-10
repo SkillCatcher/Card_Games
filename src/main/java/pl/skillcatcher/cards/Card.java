3a9c4244fb46ddcc17606b9aceafcae4cdbb056f
SkillCatcher <marek.rudkiewicz@gmail.com>
2018-05-10 05:18:17 +0200

    Changed class Card

--- src/main/java/pl/skillcatcher/cards/Card.java
+++ src/main/java/pl/skillcatcher/cards/Card.java
@@ -4,14 +4,12 @@ public class Card {
 
     private String name;
     private int value;
-    private Enum<Number> number;
-    private Enum<Color> color;
+    private int id;
 
-    public Card(Enum<Number> number, Enum<Color> color) {
-        this.number = number;
-        this.color = color;
-        this.name = number.toString() + " of " + color.toString();
+    public Card(int id) {
+        this.name = determineNumber(id) + " of " + determineColor(id);
         this.value = 0;
+        this.id = id;
     }
 
     public String getName() {
@@ -22,15 +20,59 @@ public class Card {
         return value;
     }
 
-    public Enum<Number> getNumber() {
-        return number;
-    }
-
-    public Enum<Color> getColor() {
-        return color;
+    public int getId() {
+        return id;
     }
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    private String determineNumber(int i) {
+        switch(i/4) {
+            case 12:
+                return "Ace";
+            case 0:
+                return "Two";
+            case 1:
+                return "Three";
+            case 2:
+                return "Four";
+            case 3:
+                return "Five";
+            case 4:
+                return "Six";
+            case 5:
+                return "Seven";
+            case 6:
+                return "Eight";
+            case 7:
+                return "Nine";
+            case 8:
+                return "Ten";
+            case 9:
+                return "Jack";
+            case 10:
+                return "Queen";
+            case 11:
+                return "King";
+            default:
+                return "Error";
+        }
+    }
+
+    private String determineColor(int i) {
+        switch (i%4) {
+            case 0:
+                return "Clubs";
+            case 1:
+                return "Diamonds";
+            case 2:
+                return "Hearts";
+            case 3:
+                return "Spades";
+            default:
+                return "Error";
+        }
     }
 }

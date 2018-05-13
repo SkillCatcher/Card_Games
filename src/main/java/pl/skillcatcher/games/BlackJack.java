d9d8b890b5838cad75b10a19d57175e5bb86cfbb
SkillCatcher <marek.rudkiewicz@gmail.com>
2018-05-13 19:41:53 +0200

    Defined options of user actions in Black Jack game

--- src/main/java/pl/skillcatcher/games/BlackJack.java
+++ src/main/java/pl/skillcatcher/games/BlackJack.java
@@ -9,8 +9,6 @@ import java.util.Scanner;
 public class BlackJack {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
         List<Card> cardDeck = new ArrayList<Card>();
         List<Card> playerHand = new ArrayList<Card>();
         List<Card> dealerHand = new ArrayList<Card>();
@@ -31,6 +29,11 @@ public class BlackJack {
         dealACard(cardDeck, playerHand);
         dealACard(cardDeck, dealerHand);
         dealACard(cardDeck, playerHand);
+
+        System.out.println("You can see, that dealer's got a " + dealerHand.get(0).getName() + ".");
+
+        playersChoiceMenu(playerHand, cardDeck);
+
     }
 
     private static void dealACard(List<Card> deck, List<Card> receivingHand) {
@@ -38,6 +41,60 @@ public class BlackJack {
         Card randomCard = deck.get(randomCardInDeck);
         deck.remove(randomCardInDeck);
         receivingHand.add(randomCard);
+    }
+
+    private static int getTotalPoints(List<Card> hand) {
+        int sum = 0;
+
+        for(Card card : hand) {
+            sum += card.getValue();
+        }
+
+        return sum;
+    }
+
+    private static void playersChoiceMenu(List<Card> playersHand, List<Card> deck) {
+
+        System.out.println("Your current hand: ");
+
+        for (int i = 0; i < playersHand.size(); i++) {
+            System.out.println(i+1 + ". " + playersHand.get(i).getName());
+        }
+
+        System.out.println("Your current points: " + getTotalPoints(playersHand));
+        System.out.println("");
+
+        //System.out.println("You can see, that dealer's got a " + dealersHand.get(0).getName() + ".");
+
+        if(getTotalPoints(playersHand) > 21) {
+            System.out.println("Oops... your currently have " + getTotalPoints(playersHand) +
+                    " points, which is more than 21. You've lost");
+        } else {
+            playersChoice(playersHand, deck);
+        }
+
+    }
+
+    private static void playersChoice(List<Card> playersHand, List<Card> deck) {
+        System.out.println("Do you want a hit or do you want to stay? [Press 1 or 2, and confirm with ENTER]");
+        System.out.println("1 - Hit me!");
+        System.out.println("2 - I'm good - I'll stay.");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        if(choice == 1) {
+            dealACard(deck, playersHand);
+            playersChoiceMenu(playersHand, deck);
+        } else if (choice == 2) {
+            System.out.println("You've finished with " + getTotalPoints(playersHand) + " points.");
+            System.out.println("Now it's dealer's turn...");
+        } else {
+            System.out.println("Wrong command - try again...");
+            System.out.println("");
+            playersChoice(playersHand, deck);
+        }
+
     }
 
     //player move (hit/stand)

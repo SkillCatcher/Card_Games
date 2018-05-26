6cfce3bbbe155ddb2198b36ed3fd137edbac76ab
SkillCatcher <marek.rudkiewicz@gmail.com>
2018-05-26 10:07:04 +0200

    Updated GameRunner - now user inputs the number of rounds

--- src/main/java/pl/skillcatcher/games/GameRunner.java
+++ src/main/java/pl/skillcatcher/games/GameRunner.java
@@ -1,9 +1,15 @@
 package pl.skillcatcher.games;
 
+import java.util.Scanner;
+
 public class GameRunner {
     public static void main(String[] args) {
 
-        BlackJack game = new BlackJack(5);
+        System.out.println("Please insert the number of rounds you want to play");
+        Scanner scanner = new Scanner(System.in);
+        int rounds = scanner.nextInt();
+
+        BlackJack game = new BlackJack(rounds);
         game.startTheGame();
     }
 }

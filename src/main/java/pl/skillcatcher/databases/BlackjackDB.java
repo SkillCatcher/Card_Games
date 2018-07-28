bb49e7b51ee13d4878f6d5d4f628872ef8a08d7f
SkillCatcher <marek.rudkiewicz@gmail.com>
2018-07-28 21:59:56 +0200

    Created basics of database for BlackJack game in a separate package

--- src/main/java/pl/skillcatcher/databases/BlackjackDB.java
+++ src/main/java/pl/skillcatcher/databases/BlackjackDB.java
@@ -1,4 +1,42 @@
 package pl.skillcatcher.databases;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class BlackjackDB {
+    public final String DB_NAME = "blackjackResults.db";
+    public final String CONNECTION_STRING = "jdbc:h2:/C:/Users/SkillCatcher/IdeaProjects/Card_Games/" + DB_NAME;
+
+    public final String TABLE_CURRENT_GAME = "BlackJackGame";
+
+    public final String COLUMN_ROUND = "Round";
+    public final String COLUMN_DEALER = "Dealer";
+    public final String[] COLUMN_PLAYERS;
+
+    public BlackjackDB(String[] COLUMN_PLAYERS) {
+        this.COLUMN_PLAYERS = COLUMN_PLAYERS;
+    }
+
+    private Connection connection;
+
+    public boolean open() {
+        try {
+            connection = DriverManager.getConnection(CONNECTION_STRING);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Can't connect to database - " + e.getMessage());
+            return false;
+        }
+    }
+
+    public void close() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Can't close the database - " + e.getMessage());
+        }
+    }
 }
